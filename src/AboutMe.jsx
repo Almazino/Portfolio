e2091144b231ffd7aaf8fc434e2012cadc90488e
Almazino <116052294+Almazino@users.noreply.github.com>
2023-05-24 19:10:48 +0600

    Add files via upload

---
+++ src/AboutMe.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './AboutMe.css'; // Подключение файла стилей
+
+const AboutMe = () => {
+  return (
+    <div className="container">
+       <h2>About Me</h2>
+  <p>Hi, I'm Almaz, a passionate full-stack developer with a love for creating innovative web applications.</p>
+  <p>I have a strong foundation in HTML, CSS, and JavaScript, and I enjoy building user-friendly and responsive interfaces.</p>
+  <p>With a keen interest in learning and keeping up with the latest technologies, I'm constantly expanding my skills to deliver high-quality code.</p>
+  <p>I'm a dedicated problem solver and a team player, always striving to contribute to impactful projects and collaborate with other talented individuals.</p>
+
+    </div>
+  );
+}
+
+export default AboutMe;
